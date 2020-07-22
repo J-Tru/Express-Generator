9ea4ef0130cab47cdb7b0370a519d0d7e5616d0e
Jesus T <flareon831@gmail.com>
2020-07-21 22:19:08 -0700

    workshopt 3

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const authenticate = require('../authenticate');
+const { verify } = require('jsonwebtoken');
 
 
 const partnerRouter = express.Router();
@@ -7,45 +9,73 @@ const partnerRouter = express.Router();
 partnerRouter.use(bodyParser.json());
 
 partnerRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+.get((req, res,next) => {
+    Partner.find()
+    .then(partners => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partners);
+    })
+    .catch(err => next(err));
 })
-.get((req, res) => {
-    res.end('Will send all the partners to you');
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.create(req.body)
+    .then(partner => {
+        console.log('Partner Created ', partner);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
 })
-.post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
-.put((req, res) => {
+.put(authenticate.verifyUser,(req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /partners');
+    res.end('PUT operation not supported on /campsites');
 })
-.delete((req, res) => {
-    res.end('Deleting all partners');
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 partnerRouter.route('/:partnerId')
-    .all((req, res, next) => {
+.get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+    .then(partner => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
     })
-    .get((req, res) => {
-        res.end(`Will send details of the campsite: ${req.params.partnerId} to you`);
+    .catch(err => next(err));
+})
+.post(authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.findByIdAndUpdate(req.params.partnerId, {
+        $set: req.body
+    }, { new: true })
+    .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
     })
-    .post((req, res) => {
-        res.statusCode = 403;
-        res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
+    .catch(err => next(err));
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.findByIdAndDelete(req.params.parnterId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
     })
-    .put((req, res) => {
-        res.write(`Updating the partner: ${req.params.partnerId}\n`);
-        res.end(`Will update the partner: ${req.body.name} with description: ${req.body.description}`);
-    })
-    .delete((req, res) => {
-        res.end(`Deleting partner: ${req.params.partnerId}`);
-    });
+    .catch(err => next(err));
+});
 
    
 
